d1b50e4b55eeaa9e3722bc623dce887fe528faa3
Samir Comprés <samirant15@gmail.com>
2025-07-13 11:47:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
+
+  # Ingress: Restrict to specific trusted IPs (e.g., your office/home IP)
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["203.0.113.0/32"]
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+
+  # Egress: Still allows outbound to install packages, but can also be tightened if needed
   egress {
     from_port   = 0
     to_port     = 0
