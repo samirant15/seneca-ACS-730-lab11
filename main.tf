06ce2ac79d42dc1307eeb2db30f2efdf372ea6e8
Samir Comprés <samirant15@gmail.com>
2025-07-13 11:50:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ resource "aws_security_group" "web-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["203.0.113.0/32"]
   }
 }
