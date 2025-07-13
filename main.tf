c4584d4f18ff68869bd12dc8ebfb5c007c1f56da
Samir Comprés <samirant15@gmail.com>
2025-07-13 11:49:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
 
-  # Ingress: Restrict to specific trusted IPs (e.g., your office/home IP)
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -9,11 +8,10 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["203.0.113.0/32"]
   }
 
-  # Egress: Still allows outbound to install packages, but can also be tightened if needed
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
